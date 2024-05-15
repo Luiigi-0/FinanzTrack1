6e1825be29fbb33a74a33111a49a531c32657487
Dhens-0111 <mazuerasofia57@gmail.com>
2024-05-15 14:59:28 -0500

    inicio nuevo
    diseño pagina inicio

---
+++ home/oper.js
@@ -0,0 +1,8 @@
+var num;
+egresos = num - [, , , ,];
+otros_ingresos = ingresos + otros_ingresos;
+
+
+ingresos = ("Sus ingresos mensuales son: ", num);
+otros_ingresos = ("");
+egresos = ("La cantida de dinero que tiene disponible al momento es: ", [num]);
